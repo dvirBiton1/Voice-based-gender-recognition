07b27f5f8400167496b828abfabb65db5f3c6339
dvirBiton1 <dvir199710@gmail.com>
2022-11-01 10:28:01 +0200

    update

--- Code/ModelsTrainer.py
+++ Code/ModelsTrainer.py
@@ -2,7 +2,7 @@ import os
 import pickle
 import warnings
 import numpy as np
-from sklearn.mixture import GMM
+from sklearn import mixture
 from FeaturesExtractor import FeaturesExtractor
 
 warnings.filterwarnings("ignore")
@@ -12,29 +12,35 @@ class ModelsTrainer:
 
     def __init__(self, females_files_path, males_files_path):
         self.females_training_path = females_files_path
-        self.males_training_path   = males_files_path
-        self.features_extractor    = FeaturesExtractor()
+        self.males_training_path = males_files_path
+        self.features_extractor = FeaturesExtractor()
 
     def process(self):
         females, males = self.get_file_paths(self.females_training_path,
                                              self.males_training_path)
         # collect voice features
         female_voice_features = self.collect_features(females)
-        male_voice_features   = self.collect_features(males)
+        male_voice_features = self.collect_features(males)
         # generate gaussian mixture models
-        females_gmm = GMM(n_components = 16, n_iter = 200, covariance_type='diag', n_init = 3)
-        males_gmm   = GMM(n_components = 16, n_iter = 200, covariance_type='diag', n_init = 3)
+        females_gmm = mixture.GaussianMixture(n_components=16, covariance_type='diag', n_init=3)
+        males_gmm = mixture.GaussianMixture(n_components=16, covariance_type='diag', n_init=3)
+        # females_gmm.means_ = 16, 200
+        # males_gmm.means_ = 16, 200
+        # print(females_gmm.means_)
         # fit features to models
-        females_gmm.fit(female_voice_features)
-        males_gmm.fit(male_voice_features)
+        try:
+            females_gmm.fit(female_voice_features)
+            males_gmm.fit(male_voice_features)
+        except:
+            print("Error!")
         # save models
         self.save_gmm(females_gmm, "females")
-        self.save_gmm(males_gmm,   "males")
+        self.save_gmm(males_gmm, "males")
 
     def get_file_paths(self, females_training_path, males_training_path):
         # get file paths
-        females = [ os.path.join(females_training_path, f) for f in os.listdir(females_training_path) ]
-        males   = [ os.path.join(males_training_path, f) for f in os.listdir(males_training_path) ]
+        females = [os.path.join(females_training_path, f) for f in os.listdir(females_training_path)]
+        males = [os.path.join(males_training_path, f) for f in os.listdir(males_training_path)]
         return females, males
 
     def collect_features(self, files):
@@ -52,10 +58,12 @@ class ModelsTrainer:
         for file in files:
             print("%5s %10s" % ("PROCESSNG ", file))
             # extract MFCC & delta MFCC features from audio
-            vector    = self.features_extractor.extract_features(file)
+            vector = self.features_extractor.extract_features(file)
             # stack the features
-            if features.size == 0:  features = vector
-            else:                   features = np.vstack((features, vector))
+            if features.size == 0:
+                features = vector
+            else:
+                features = np.vstack((features, vector))
         return features
 
     def save_gmm(self, gmm, name):
@@ -68,9 +76,9 @@ class ModelsTrainer:
         filename = name + ".gmm"
         with open(filename, 'wb') as gmm_file:
             pickle.dump(gmm, gmm_file)
-        print ("%5s %10s" % ("SAVING", filename,))
+        print("%5s %10s" % ("SAVING", filename,))
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     models_trainer = ModelsTrainer("TrainingData/females", "TrainingData/males")
     models_trainer.process()
