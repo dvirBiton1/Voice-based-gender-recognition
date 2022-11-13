30828d225d844ee3c6b71e83bf6bbf28fc1c5e63
Dvir Biton <92736991+dvirBiton1@users.noreply.github.com>
2022-11-13 09:02:05 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from sklearn import mixture
-
-females_gmm = mixture.GaussianMixture(n_components=16, means_init=(200,200), covariance_type='diag', n_init=3)
-males_gmm = mixture.GaussianMixture(n_components=16, means_init=200, covariance_type='diag', n_init=3)
-females_gmm.means_= 16,200
-print(females_gmm.means_)
